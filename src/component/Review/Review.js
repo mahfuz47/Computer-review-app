8ec0cc6f3e1f4e0048dd5e92074900e852b1254c
mahfuz47 <mahfuzshikder560@gmail.com>
2022-04-05 00:07:09 +0600

    review data edited done

--- src/component/Review/Review.js
+++ src/component/Review/Review.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Review = (props) => {
   const { name, opinion, Ratings,img} = props.review;
   return (
-    <div className="review-container w-96 h-96 border border-gray-400 rounded-xl p-1 text-center">
+    <div className="review-container w-96 h-100 border border-gray-400 rounded-xl p-1 text-center">
       <img className="w-96 rounded-xl" src={img} alt="" />
-      <h1>{name}</h1>
-      <h2>opinion: {opinion}</h2>
+      <h1 className=" font-semibold text-2xl font-mono">{name}</h1>
+      <h2>
+        <span className=' font-semibold font-mono'>OPINION:</span> {opinion}
+      </h2>
       <h3>ratings:{Ratings}</h3>
     </div>
   );
